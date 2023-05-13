b9ec5c88a4a618c811441c1e9b9e07500b594092
Bianca Louise Dantic <biancalouisedantic@gmail.com>
2023-05-13 10:07:50 +0800

    first commit

---
+++ forLoopNames.js
@@ -0,0 +1,11 @@
+const classNames = ['Aveunalliv', 'Abelle', 'Christian','Dominic','Edward','Ian','Jei','Jerome','Bianca','Jheric','Jim','John','Johnroldan','Jomell','Joven']
+const myName = "Bianca"
+for(let i = 0 ; i <= classNames.length; i++){
+  console.log(classNames[i]);
+
+if (classNames[i] === myName){
+  break;
+}
+
+
+}
---
+++ functions.js
@@ -0,0 +1,8 @@
+
+//functions
+function convertCelsiusToFahrenheit(celsiusTemperature){
+  return celsiusTemperature * 1.8 + 32
+}
+console.log(convertCelsiusToFahrenheit(32));
+console.log(convertCelsiusToFahrenheit(38));
+console.log(convertCelsiusToFahrenheit(1));
