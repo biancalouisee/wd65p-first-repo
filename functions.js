ae8df6ceef8cb0db7f0c5e1475307f067abe3acc
Bianca Louise Dantic <128367644+biancalouisee@users.noreply.github.com>
2023-05-13 10:47:31 +0800

    Delete functions.js

--- functions.js
+++
@@ -1,8 +0,0 @@
-
-//functions
-function convertCelsiusToFahrenheit(celsiusTemperature){
-  return celsiusTemperature * 1.8 + 32
-}
-console.log(convertCelsiusToFahrenheit(32));
-console.log(convertCelsiusToFahrenheit(38));
-console.log(convertCelsiusToFahrenheit(1));
